2a58836c78368715cc459bc9a440143bc59f00f0
Luna <git@l4.pm>
2023-04-22 19:32:37 -0300

    add generation ui

---
+++ synthnav/generate.py
@@ -0,0 +1,86 @@
+import asyncio
+import json
+import random
+import logging
+import string
+import os
+import websockets
+
+log = logging.getLogger(__name__)
+
+# Gradio changes this index from time to time. To rediscover it, set VISIBLE = False in
+# modules/api.py and use the dev tools to inspect the request made after clicking on the
+# button called "Run" at the bottom of the UI
+GRADIO_FN = 43
+
+
+def random_hash():
+    letters = string.ascii_lowercase + string.digits
+    return "".join(random.choice(letters) for i in range(9))
+
+
+async def generate_text(input_prompt):
+    server = os.environ["SERVER_ADDR"]
+    params = {
+        "max_new_tokens": 200,
+        "do_sample": True,
+        "temperature": 0.72,
+        "top_p": 0.73,
+        "typical_p": 1,
+        "repetition_penalty": 1.1,
+        "encoder_repetition_penalty": 1.0,
+        "top_k": 0,
+        "min_length": 0,
+        "no_repeat_ngram_size": 0,
+        "num_beams": 1,
+        "penalty_alpha": 0,
+        "length_penalty": 1,
+        "early_stopping": False,
+        "seed": -1,
+        "add_bos_token": True,
+        "truncation_length": 2048,
+        "ban_eos_token": False,
+        "skip_special_tokens": True,
+        "stopping_strings": [],
+    }
+    payload = json.dumps([input_prompt, params])
+    session = random_hash()
+    url = f"ws://{server}/queue/join"
+
+    log.debug("connecting to %r", url)
+    async with websockets.connect(url) as websocket:
+        log.debug("connected to %r", url)
+        while content := json.loads(await websocket.recv()):
+            log.debug("got msg %r", content)
+            # Python3.10 syntax, replace with if elif on older
+            match content["msg"]:
+                case "send_hash":
+                    await websocket.send(
+                        json.dumps({"session_hash": session, "fn_index": GRADIO_FN})
+                    )
+                case "estimation":
+                    pass
+                case "send_data":
+                    log.debug("sending data %r", payload)
+                    await websocket.send(
+                        json.dumps(
+                            {
+                                "session_hash": session,
+                                "event_data": None,
+                                "fn_index": GRADIO_FN,
+                                "data": [payload],
+                            }
+                        )
+                    )
+                case "process_starts":
+                    pass
+                case "process_generating" | "process_completed":
+                    log.debug("got %r", content)
+                    yield content["output"]["data"][0]
+                    # You can search for your desired end indicator and
+                    #  stop generation by closing the websocket here
+                    if content["msg"] == "process_completed":
+                        log.debug("complete")
+                        break
+
+        log.debug("reached end of stream, returning")
--- synthnav/main.py
+++ synthnav/main.py
@@ -1,8 +1,11 @@
+import aiohttp
 import os
 import logging
 import asyncio
+from dataclasses import dataclass
 import tkinter as tk
 from tkinter import ttk
+from .generate import generate_text
 
 
 log = logging.getLogger(__name__)
@@ -10,8 +13,8 @@ log = logging.getLogger(__name__)
 # asyncio tkinter trick taken from
 # https://www.loekvandenouweland.com/content/python-asyncio-and-tkinter.html
 class App:
-    async def run_forever(self):
-        self.window = Window()
+    async def run_forever(self, ctx):
+        self.window = Window(ctx)
         try:
             await self.window.update_forever()
         except asyncio.CancelledError:
@@ -19,8 +22,31 @@ class App:
 
 
 class Window(tk.Tk):
-    def __init__(self):
+    def __init__(self, ctx):
         self.root = tk.Tk()
+
+        self.prompt_widget = tk.Entry(self.root)
+        self.prompt_widget.pack()
+
+        self.prompt_text = tk.StringVar()
+        self.prompt_widget["textvariable"] = self.prompt_text
+        self.prompt_widget.bind("<Key-Return>", self.generate_prompt)
+
+        self.output_text = tk.StringVar()
+        self.output_widget = tk.Label(
+            self.root, textvariable=self.output_text, wraplength=512
+        )
+        self.output_widget.pack()
+
+    def generate_prompt(self, event):
+        log.info("prompt: %r", self.prompt_text.get())
+        asyncio.create_task(self._generate(self.prompt_text.get()))
+
+    async def _generate(self, prompt):
+        assert prompt is not None
+        async for incoming_response in generate_text(prompt):
+            log.debug("incoming response: %r", incoming_response)
+            self.output_text.set(incoming_response)
 
     async def update_forever(self):
         while True:
@@ -28,11 +54,16 @@ class Window(tk.Tk):
             await asyncio.sleep(0.1)
 
 
+@dataclass
+class Context:
+    http_client: aiohttp.ClientSession
+
+
 def main():
     logging.basicConfig(
         level=logging.DEBUG if os.environ.get("DEBUG") else logging.INFO
     )
     log.info("boot")
-
-    asyncio.run(App().run_forever())
+    ctx = Context(None)
+    asyncio.run(App().run_forever(ctx))
     log.info("shutdown")
